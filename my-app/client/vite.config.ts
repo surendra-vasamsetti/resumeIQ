c06ca1e4c63503c88ef494fdc95efcd9900c24e6
surendra-vasamsetti <leelasurendravasamsetti@gmamil.com>
2025-07-06 22:42:45 +0530

    deployment-update

--- my-app/client/vite.config.ts
+++ my-app/client/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ✅ Add this line for deployment
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
